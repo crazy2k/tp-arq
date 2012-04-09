1287c29482b4dcff6921f64b308ac2927f3e079e
Pablo Antonio <pablo@mabel-desktop.(none)>
2012-04-09 06:25:00 -0300

    Make arqsimucpu allow some parallelism

--- arqsimucpu.cpp
+++ arqsimucpu.cpp
@@ -1,55 +1,144 @@
 #include "arqsimucache.hpp"
 #include "arqsimujumps.hpp"
+#include <algorithm>
 
+static VOID process_memread_wrap(VOID *ip, VOID *addr);
 
-static std::ofstream outfile;
-static Memory *front_memory;
-static UINT64 cycles;
-static UINT64 instrs;
-static Predictor *predictor;
+static VOID process_condbranch_wrap(VOID *ip, VOID *target, bool taken);
 
-static VOID rec_memread(VOID *ip, VOID *addr) {
-    cycles += front_memory->read(addr);
-}
+class CPU {
+    private:
+        UINT64 cycles;
+        UINT64 instrs;
 
-static VOID analyze_condbranch(VOID *ip, VOID *target, bool taken) {
-    if (!(predictor->analyze(ip, target, taken)))
-        cycles += 5;
-}
+        Memory *front_memory;
+        Predictor *predictor;
 
-static VOID instrument_instruction(INS ins, VOID *v) {
-    instrs++;
+        // Mechanism that allows a certain amount of parallelism while reading
+        // from memory
+        list<REG> recent_regs;
+        UINT64 spare_cycles;
 
-    if (INS_IsMemoryRead(ins)) {
-        UINT32 memops = INS_MemoryOperandCount(ins);
+        VOID consume_cycles(UINT64 n, bool can_exec_in_parallel) {
+            if (n == 0)
+                return;
 
-        for (UINT32 memop = 0; memop < memops; memop++) {
-            if (INS_MemoryOperandIsRead(ins, memop)) {
-                INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)rec_memread,
-                    IARG_INST_PTR, IARG_MEMORYOP_EA, memop, IARG_END);
+            if (can_exec_in_parallel) {
+                if (spare_cycles > n)
+                    spare_cycles -= n;
+                else
+                    spare_cycles = 0;
             }
+            // if it's a memory operation or another operation that reads
+            // or writes to a register being used by a memory operation,
+            // then it comes here
+            else {
+                // if that's the case, spare cycles have to be consumed 
+                if (spare_cycles > 0)
+                    cycles += spare_cycles;
+            }
+
+            cycles += n;
         }
-    }
-    else if (INS_IsBranchOrCall(ins) && INS_HasFallThrough(ins)) {
-        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)analyze_condbranch,
-            IARG_INST_PTR, IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN, IARG_END);
-    }
-    else {
-        // if the instruction doesn't either read memory or is a conditional
-        // branch, it counts as one cycle (writes are assumed to eat one cycle
-        // too)
-        cycles++;
-    }
-}
 
-static VOID finalize(INT32 code, VOID *v) {
-    outfile << "\tcycles/instructions: " <<
-        uint_to_string(cycles) << " / " << uint_to_string(instrs) <<
-        " = " << double_to_string(cycles/(double)instrs) <<
-        std::endl;
+        VOID process_memop(UINT64 op_cycles) {
+            // if a memory operation is being done, we have to wait for it to
+            // finish in order to perform the read
+            consume_cycles(1, false);
+            spare_cycles = op_cycles - 1;
+        }
 
-    outfile.close();
-}
+    public:
+        CPU(Memory *pfront_memory, Predictor *ppredictor) :
+            front_memory(pfront_memory), predictor(ppredictor) {
+            cycles = 0;
+            instrs = 0;
+            spare_cycles = 0;
+        }
+
+        VOID process_memread(VOID *ip, VOID *addr) {
+            process_memop(front_memory->read(addr));
+        }
+
+        VOID process_condbranch(VOID *ip, VOID *target, bool taken) {
+            if (predictor->analyze(ip, target, taken))
+                consume_cycles(1, true);
+            else
+                consume_cycles(5, true);
+        }
+
+
+
+        VOID process_instruction(INS ins) {
+            instrs++;
+
+            // if it's a memory read,
+            if (INS_IsMemoryRead(ins)) {
+                UINT32 memops = INS_MemoryOperandCount(ins);
+
+                // process each address separately
+                for (UINT32 memop = 0; memop < memops; memop++) {
+                    if (INS_MemoryOperandIsRead(ins, memop)) {
+                        INS_InsertPredicatedCall(ins, IPOINT_BEFORE,
+                            (AFUNPTR)process_memread_wrap, IARG_INST_PTR,
+                            IARG_MEMORYOP_EA, memop, IARG_END);
+                    }
+                }
+
+                // record registers that are being written
+                recent_regs.clear();
+                UINT32 wregs = INS_MaxNumWRegs(ins);
+                for (UINT32 wreg = 0; wreg < wregs; wreg++)
+                    recent_regs.push_back(INS_RegW(ins, wreg));
+            }
+            // if it's a memory write,
+            if (INS_IsMemoryWrite(ins)) {
+                // only use one cycle (but since it's a memory operation, it
+                // will have to wait for other memory operations to complete
+                process_memop(1);
+            }
+            else if (INS_IsBranchOrCall(ins) && INS_HasFallThrough(ins)) {
+                INS_InsertPredicatedCall(ins, IPOINT_BEFORE,
+                    (AFUNPTR)process_condbranch_wrap, IARG_INST_PTR,
+                    IARG_BRANCH_TARGET_ADDR, IARG_BRANCH_TAKEN,
+                    IARG_END);
+            }
+            else {
+                // if the instruction doesn't either read or write to memory
+                // nor is a conditional branch, it consumes one cycle and can
+                // be executed in parallel if it doesn't need a currently
+                // being used register
+                consume_cycles(1, true);
+
+                // check whether the instruction uses a register that's being
+                // written to by a memory operation
+                bool uses_recent_reg = false;
+
+                UINT32 wregs = INS_MaxNumWRegs(ins);
+                for (UINT32 reg = 0; reg < wregs; reg++)
+                    if (count(recent_regs.begin(), recent_regs.end(),
+                        INS_RegW(ins, reg)))
+                        uses_recent_reg = true;
+
+                UINT32 rregs = INS_MaxNumRRegs(ins);
+                for (UINT32 reg = 0; reg < rregs; reg++)
+                    if (count(recent_regs.begin(), recent_regs.end(),
+                        INS_RegR(ins, reg)))
+                        uses_recent_reg = true;
+
+                // it can be excecuted in parallel if it doesn't
+                consume_cycles(1, !uses_recent_reg);
+            }
+
+        }
+
+        VOID output(std::ostream *outstream) {
+            *outstream << "\tcycles/instructions: " <<
+                uint_to_string(cycles) << " / " << uint_to_string(instrs) <<
+                " = " << double_to_string(cycles/(double)instrs) <<
+                std::endl;
+        }
+};
 
 static INT32 usage() {
     PIN_ERROR("This Pintool simulates a cache hierarchy\n" +
@@ -57,13 +146,32 @@ static INT32 usage() {
     return -1;
 }
 
+static std::ofstream outfile;
+static CPU *cpu;
+
+static VOID instrument_instruction(INS ins, VOID *v) {
+    cpu->process_instruction(ins);
+}
+
+static VOID finalize(INT32 code, VOID *v) {
+    cpu->output(&outfile);
+
+    outfile.close();
+}
+
+static VOID process_memread_wrap(VOID *ip, VOID *addr) {
+    cpu->process_memread(ip, addr);
+}
+
+static VOID process_condbranch_wrap(VOID *ip, VOID *target, bool taken) {
+    cpu->process_condbranch(ip, target, taken);
+}
 
 int main(int argc, char *argv[])
 {
 
     if (PIN_Init(argc, argv))
         return usage();
-
 
     outfile.open("arqsimucpu.out");
 
@@ -75,12 +183,9 @@ int main(int argc, char *argv[])
     Cache *l2 = new Cache("L2", ram, 1000*1024, 2, 16);
     Cache *l1 = new Cache("L1", l2, 64*1024, 2, 16);
 
-    front_memory = l1;
+    Predictor *predictor = new HistoryPredictor(2);
 
-    predictor = new HistoryPredictor(2);
-
-    cycles = 0;
-    instrs = 0;
+    cpu = new CPU(l1, predictor);
 
     // start program and never return
     PIN_StartProgram();
